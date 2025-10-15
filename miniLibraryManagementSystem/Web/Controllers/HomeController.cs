eebc77e9cd2e8a62d605a9878ddbb59cce7150b0
Hady Hassan <99578132+hady444@users.noreply.github.com>
2025-10-15 21:24:49 +0300

    Adding HomeViewModel to display statistics.

--- miniLibraryManagementSystem/Web/Controllers/HomeController.cs
+++ miniLibraryManagementSystem/Web/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using miniLibraryManagementSystem.Web.ViewModels;
+using System.Diagnostics;
+using miniLibraryManagementSystem.Services;
 
-namespace miniLibraryManagementSystem.Web.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBookService _bookService;
+        private readonly IAuthorService _authorService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBookService bookService, IAuthorService authorService)
         {
             _logger = logger;
+            _bookService = bookService;
+            _authorService = authorService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var model = new HomeViewModel();
+
+            var allBooks = (await _bookService.GetAllAsync()).Count();
+            var availableBooks = (await _bookService.GetAllAsync()).Where(b => !b.IsBorrowed).Count();
+            model.TotalBooks = allBooks; // Example data
+            model.TotalAuthors = (await _authorService.GetAllAsync()).Count(); // Example data
+            model.BorrowedBooks = allBooks - availableBooks; // Example data
+            model.AvailableBooks = availableBooks; // Example data
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ miniLibraryManagementSystem/Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace miniLibraryManagementSystem.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public int TotalBooks { get; set; }
+        public int TotalAuthors { get; set; }
+        public int BorrowedBooks { get; set; }
+        public int AvailableBooks { get; set; }
+
+    }
+}
